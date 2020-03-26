60f9329085bd8f23f941384aebca9bc761e0c2b2
FreeA7 <987320967@qq.com>
2020-03-26 13:06:02 +0800

    longest peak list

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ def timer(func):
         value = func(*args, **kwargs)
         end_time = time.perf_counter()
         run_time = end_time - begin_time
-        print('{} 共用时：{} s'.format(func.__name__, run_time))
+        print('{} 共用时：{} s'.format(func.__qualname__, run_time))
         return value
     return wrapper
 
