6801299febe7f3598f3bb2d8fdb83e432f87279f
FreeA7 <987320967@qq.com>
2020-03-29 01:21:29 +0800

    kill_monster bitmasking+dp

--- kill_monster.py
+++ kill_monster.py
@@ -5,31 +5,59 @@ Created on Sat Mar 28 20:53:15 2020
 @author: 98732
 """
 
-class Solution:
-    def getBit(self, mask, n):
-        output = []
-        for i in range(n-1, -1, -1):
-            res = mask % (2**i)
-            if res == mask:
-                output.append(0)
-            else:
-                output.append(1)
-            mask = res
-        for i in range(len(output)-1, -1, -1):
-            yield output[i]
-    
+
+from utils import timer
+import random
+
+# Bitmasking & bp O(2^n*n)
+class Solution1:
+    @timer
     def attackMonster(self , monsterLength , monsterPoint):
         inf = float('inf')
         monsterPoint = sorted(monsterPoint)
         dp = [inf for _ in range(2**len(monsterPoint))]
-        for i in range(0, len(monsterPoint)):
-            dp[2**i] = monsterLength
+        dp[0] = 0
         for mask in range(2**len(monsterPoint)):
             for k in range(len(monsterPoint)):
                 if not mask&(1<<k):
-                    
-                    for bit in self.getBit(mask, k):
-                        
-
-                            
+                    plus = monsterLength
+                    max_j = -1
+                    for j in range(len(monsterPoint)):
+                        if j == k:
+                            if max_j != -1:
+                                plus -= monsterPoint[max_j]
+                            break
+                        elif mask&(1<<j):
+                            max_j = j
+                    max_j = -1
+                    for j in range(len(monsterPoint)-1, -1, -1):
+                        if j == k:
+                            if max_j != -1:
+                                plus -= (monsterLength - monsterPoint[max_j])
+                            break
+                        elif mask&(1<<j):
+                            max_j = j
                     dp[mask|(1<<k)] = min([dp[mask|(1<<k)], dp[mask]+plus])
+        return dp[-1]
+        
+
+def getTest(l, n):
+    output = []
+    t = 0
+    for i in range(n):
+        while t in output:
+            t = random.randint(0, l)
+        output.append(t)
+    return [l, output]
+
+
+target1 = [20,[2,5,10,18]]
+target2 = getTest(100, 15)
+
+tar = target2
+
+s = Solution1()
+print(s.attackMonster(tar[0], tar[1]))
+
+s = Solution2()
+print(s.attackMonster(tar[0], tar[1]))
